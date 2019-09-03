f3fc89e2a5b128158e7dd620a89b6aa5e01ddd77
Toutatis <search@Toutatiss-Mac-mini.local>
2019-09-02 18:18:39 -0700

    add css and user sumbit button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, request, url_for
+from flask import Flask, render_template, session, redirect, request, url_for, g
 from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
 from user import User
 from database import Database
@@ -8,6 +8,13 @@ app.secret_key = '1234'
 
 Database.initialise(user='postgres', password='menliang99', host='localhost', database='learning')
 
+
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db_by_screen_name(session['screen_name'])
+
+
 @app.route('/')
 def homepage():
     return render_template('home.html')
@@ -15,10 +22,18 @@ def homepage():
 
 @app.route('/login/twitter')
 def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for('profile'))
     request_token = get_request_token()
     session['request_token'] = request_token
     # redirecting the user to twitter so they can confirm authorization
     return redirect(get_oauth_verifier_url(request_token))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/auth/twitter')
@@ -36,8 +51,19 @@ def twitter_auth():
     session['screen_name'] = user.screen_name
     return redirect(url_for('profile'))
 
+
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', screen_name=session['screen_name'])
+    return render_template('profile.html', user=g.user)
+
+
+@app.route('/search')   #http://127.0.0.1:4995/search?q=cars+filter:images
+def search():
+    query = request.args.get('q')
+    tweets = g.user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q={}'.format(query))
+    tweet_texts = [tweet['text'] for tweet in tweets['statuses']]
+
+    return render_template('search.html', content=tweet_texts)
+
 
 app.run(port=4995, debug=True)  # GET HTTP/2 www.api.example.com/users
